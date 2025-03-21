f37988c9c5a1f52db4e0e30038d0099abd9789b9
Madu <eduardapaiva2008@gmail.com>
2025-03-21 01:47:56 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace BuscaDinamicaProdutos
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Produtos { get; set; } = new ObservableCollection<string>
+        {
+            "Notebook",
+            "Smartphone",
+            "Tablet",
+            "Smartwatch",
+            "Fone de Ouvido",
+            "Teclado",
+            "Mouse",
+            "Monitor"
+        };
+
+        public ObservableCollection<string> ProdutosFiltrados { get; set; } = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ProdutosFiltrados = new ObservableCollection<string>(Produtos);
+            BindingContext = this;
+        }
+
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string textoBusca = e.NewTextValue?.ToLower() ?? string.Empty;
+
+            ProdutosFiltrados.Clear();
+
+            var resultados = Produtos.Where(p => p.ToLower().Contains(textoBusca));
+
+            foreach (var produto in resultados)
+            {
+                ProdutosFiltrados.Add(produto);
+            }
+        }
+    }
+}
+
